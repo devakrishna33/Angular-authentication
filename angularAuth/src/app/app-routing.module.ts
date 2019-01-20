04ddd7838458ce0a2ecba18a5e71481c1de71ffb
devakrishna33 <36110228+devakrishna33@users.noreply.github.com>
2019-01-20 23:41:02 +0530

    Addede routes

--- angularAuth/src/app/app-routing.module.ts
+++ angularAuth/src/app/app-routing.module.ts
@@ -1,7 +1,27 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { DoctorComponent } from './doctor/doctor.component';
+import { RegisterComponent } from './register/register.component';
+import { LoginComponent } from './login/login.component';
+import { CompanionComponent } from './companion/companion.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '', component: DoctorComponent, pathMatch: 'full',
+  }
+  {
+    path: 'doctor', component: DoctorComponent,
+  },
+  {
+    path: 'compainon', component: CompanionComponent,
+  },
+  {
+    path: 'register', component: RegisterComponent,
+  },
+  {
+    path: 'login', component: LoginComponent,
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- angularAuth/src/app/app.module.ts
+++ angularAuth/src/app/app.module.ts
@@ -3,10 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { RegisterComponent } from './register/register.component';
+import { LoginComponent } from './login/login.component';
+import { DoctorComponent } from './doctor/doctor.component';
+import { CompanionComponent } from './companion/companion.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    RegisterComponent,
+    LoginComponent,
+    DoctorComponent,
+    CompanionComponent
   ],
   imports: [
     BrowserModule,
