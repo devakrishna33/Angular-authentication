47d6ba9f3a96dd31f04b0a40997572e4e1b1d34f
devakrishna33 <36110228+devakrishna33@users.noreply.github.com>
2019-01-20 23:15:38 +0530

    Setup login and registration

--- server/routes/routes.js
+++ server/routes/routes.js
@@ -1,8 +1,43 @@
 const express = require('express');
 const router = express.Router();
+const UserModel = require('../models/user.model');
+const mongoose = require('mongoose');
+const db = "mongodb://devakrishna33:aaaaaaaa8@ds159377.mlab.com:59377/doctorsdb";
+mongoose.connect(db, err => {
+  console.log(err);
+});
 
 router.get('/', (req, res) => {
   res.send("Hello world");
+});
+
+router.post('/register', (req, res) => {
+  const userData = new UserModel(req.body);
+  userData.save((err, data) => {
+    if(err){
+      console.log(err);
+      return;
+    }
+    else{
+      res.status(200).send(data);
+    }
+  })
+});
+
+router.post('/login', (req, res) => {
+  UserModel.findOne({email: req.body.email}, (err, data) => {
+    if(err){
+      console.log(err);
+      return;
+    }
+    if(!data){
+      return res.status(401).send("No user found");
+    }
+    if( !(data.password == req.body.password) ){
+      return res.status(401).send("Incorrect Password");
+    }
+    return res.status(200).send("success");
+  })
 });
 
 module.exports = router;
