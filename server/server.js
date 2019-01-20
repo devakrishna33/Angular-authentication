8bd15dc5a8690cf8ec7748eab75c64b2011b8384
devakrishna33 <36110228+devakrishna33@users.noreply.github.com>
2019-01-20 22:52:03 +0530

    Setting up server

---
+++ server/server.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const routes = require('./routes/routes.js');
+
+const PORT = 3000;
+
+const app = express();
+
+//Setup body parser
+app.use(bodyParser.json());
+
+app.use('/api', routes);
+
+app.listen(PORT, () => {
+  console.log("Express server listening in port", PORT);
+});
+
+module.exports = app;
